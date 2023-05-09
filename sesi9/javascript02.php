ee6775b4c42926f3297a3fcba9aae1413acd82df
ayuaripspt <132330536+ayuaripspt@users.noreply.github.com>
2023-05-09 17:04:03 +0800

    Add files via upload

---
+++ sesi9/javascript02.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 2</title>
+</head>
+<body>
+    
+<div id = "teks1">
+    <p> pemrograman web  </p>
+</div>
+
+<div id = "teks2">
+    <p>Belajar pemrograman web selasa</p>
+</div>
+
+<script src = "javascript02.JS"></script>
+
+</body>
+</html>
